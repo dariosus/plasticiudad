55e9d0a5b4e9d5a418247e8a66c9eaacd8fe60ff
dariosus <dario15th@gmail.com>
2017-09-02 13:52:44 -0300

    Listado de metodos para productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -7,5 +7,33 @@ use App\Producto;
 
 class ProductoController extends Controller
 {
-    //
+	public function __construct() {
+		if (!User::isAllowed([2])) {
+	      return User::block();
+	    }
+	}
+
+    public function crearGet() {
+
+    }
+
+    public function crearPost(Request $request) {
+
+    }
+
+    public function eliminar($id) {
+
+    }
+
+    public function modificarGet($id) {
+
+    }
+
+    public function modificarPost(Request $req) {
+
+    }
+
+    public function listar() {
+
+    }
 }

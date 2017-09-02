ee5ee72a1d8b95c904fce342c385c599f3b137df
huenu <maradalponte@gmail.com>
2017-09-02 15:47:04 -0300

    Controller para el producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -14,27 +14,75 @@ class ProductoController extends Controller
 	    }
 	}
 
+    
     public function crearGet() {
-
+        return view("crearProducto");
     }
 
     public function crearPost(Request $request) {
+        //1 Validar
+        $reglas = [
+            "nombre" => "required|string|max:80",
+            "descripcion" => "required|string|max:500",
+            "plasticoins" => "required|integer|min:1"
+        ];
 
+        $this->validate($request, $reglas);
+
+        //2 Crear
+        $producto = new Producto;
+        $producto->nombre = $request["nombre"];
+        $producto->descripcion = $request["descripcion"];
+        $producto->plasticoins = $request["plasticoins"];
+        $producto->categoria_id = $request["categoria_id"];
+
+        $producto->save();
+
+        //3 Redirigir
+        return redirect("dashboard")->with('status', 'Producto creado');
     }
 
     public function eliminar($id) {
+        $producto = Producto::find($id);
+        $producto->delete();
 
+        return redirect("dashboard")->with('status', "El producto $producto->nombre ha sido eliminado")
     }
 
     public function modificarGet($id) {
+        $producto = Producto::find($id);
 
+        $data = compact("producto");
+
+        return view("editarProducto", $data);
     }
 
-    public function modificarPost(Request $req) {
+    public function modificarPost(Request $req, $id) {
+        //1 Validar
+        $reglas = [
+            "nombre" => "required|string|max:80",
+            "descripcion" => "required|string|max:500",
+            "plasticoins" => "required|integer|min:1"
+        ];
 
+        $this->validate($request, $reglas);
+
+        //2 Crear
+        $producto = Producto::find($id);
+        $producto->nombre = $request["nombre"];
+        $producto->descripcion = $request["descripcion"];
+
+        $producto->save();
+
+        //3 Redirigir
+        return redirect("dashboard")->with('status', 'Producto creado');
     }
 
     public function listar() {
-        
+        $productos = Producto::paginate(50);
+
+        $data = compact("productos");
+
+        return view("listarProductos", $data);
     }
 }
